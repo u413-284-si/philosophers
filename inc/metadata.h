f4432e1de034344df6949c638d79cbe2c7c6caf5
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-01 15:50:23 +0200

    feat: add input struct in metadata header

---
+++ inc/metadata.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   metadata.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 15:34:01 by sqiu              #+#    #+#             */
+/*   Updated: 2023/09/01 15:40:18 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef METADATA_H
+# define METADATA_H
+
+/* ====== DEFINITIONS ====== */
+
+typedef struct s_input
+{
+	int	num_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	req_eat;
+	int	start_time;
+}	t_input;
+
+#endif
