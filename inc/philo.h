ffb4cd2d03212bfa465f4db9e2be457775bd5592
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-01 13:17:45 +0200

    feat: add header file for main

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,22 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 19:07:06 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/29 19:07:07 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/01 12:44:55 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+
+/* ====== LIBRARIES ====== */
+
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+
+/* ====== DECLARATIONS ====== */
+
+// input check
+void	ft_check_input(int argc, char **argv);
+
+#endif
