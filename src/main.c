0cf03cf22cb4d1486f4b77b02dc582ed5280d807
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-01 12:06:44 +0200

    feat: sketch main process

--- src/main.c
+++ src/main.c
@@ -6,7 +6,26 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 19:35:18 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/28 19:35:19 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/01 12:05:45 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file main.c
+ * @brief Contains initial thread executing the main routine.
+ */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	ft_check_input(argc, argv);
+/* 	ft_init();
+	ft_feast();
+	if (philo == dead)
+		printf("Another one bites the dust\n");
+	else if (philo == fed)
+		printf("Ich bin so satt, ich mag kein Blatt\n");
+	ft_cleanup(); */
+	return (0);
+}
