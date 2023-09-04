62800a5a6242bdad88352c5bb399e206d0d0e218
sirui <sirui.qiu01@gmail.com>
2023-09-04 18:19:42 +0200

    format: instruction output

--- src/mod_err_handling.c
+++ src/mod_err_handling.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 12:29:56 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/01 15:19:23 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/04 16:30:03 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	ft_print_err_and_exit(t_err err, char *msg)
 {
 	printf("%s\n", msg);
-	if (err == ERR_ARGCOUNT || err == ERR_EMPTYSTR || err == ERR_NONNUMERIC)
+	if (err == ERR_ARGCOUNT || err == ERR_EMPTYSTR || err == ERR_NONNUMERIC
+		|| err == ERR_NEGATIVINPUT)
 		printf("Correct input: ./philo \tnumber_of_philosophers \n\
 			time_to_die \n\
-			time_to_eat time_to_sleep \n\
+			time_to_eat \n\
+			time_to_sleep \n\
 			[number_of_times_each_philosopher_must_eat] 🐥\n");
 	exit (err);
 }
