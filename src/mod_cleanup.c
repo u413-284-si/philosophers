d98da2eda070cc2aa4a6d12141c86350400eac77
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-08 12:15:51 +0200

    feat: initiate ft_destroy_philo_mutexes()

--- src/mod_cleanup.c
+++ src/mod_cleanup.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 19:33:37 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/08 11:40:18 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/08 12:12:06 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,18 @@
  */
 void	ft_cleanup(t_meta *data)
 {
-	if (data->philos)
-	{
-		free(data->philos);
-	}
 	//Check if mutex existent
 	if (pthread_mutex_destroy(&data->mtx_speak) != 0)
 		ft_print_err_and_exit(ERR_MUTEX_DESTROY, \
 		"Failed to destroy mtx_speak. 😵‍💫\n");
+	if (data->philos)
+	{
+		ft_destroy_philo_mutexes(data->philos, data->params->num_philos);
+		free(data->philos);
+	}
+}
+
+void	ft_destroy_philo_mutexes(t_philo *philos, int num_philos)
+{
+	
 }
