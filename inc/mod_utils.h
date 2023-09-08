d3114f0b1b07e8bb13f7e7b66f7c87133445dc66
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-08 15:23:33 +0200

    refactor: cleanup headers

--- inc/mod_utils.h
+++ inc/mod_utils.h
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mod_setup.h                                        :+:      :+:    :+:   */
+/*   mod_utils.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/01 15:28:33 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/08 14:05:13 by sqiu             ###   ########.fr       */
+/*   Created: 2023/09/08 14:53:45 by sqiu              #+#    #+#             */
+/*   Updated: 2023/09/08 14:55:03 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MOD_SETUP_H
-# define MOD_SETUP_H
+#ifndef MOD_UTILS_H
+# define MOD_UTILS_H
 
 /* ====== LIBRARIES ====== */
-
-# include "mod_err_handling.h"
-
-/* ====== DECLARATIONS ====== */
-int		ft_convert_str_to_num(char *str);
-void	ft_init_var(t_meta *data);
-void	ft_init_values(t_meta *data);
-
-// import from mod_utils
-long	ft_atoi(const char *str);
-void	*ft_calloc(size_t nmemb, size_t size);
-
-// import from mod_mutex
-void	ft_init_mutexes(t_meta *data);
+# include <stdlib.h>
+# include <stddef.h>
+# include <limits.h>
+# include <sys/time.h>
+# include <errno.h>
+# include <stdio.h>
 
 #endif
