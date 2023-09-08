46a38f8294d1bdf4eacd9f68c84116b03f6d3112
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-08 12:51:12 +0200

    feat: mutex init complete

--- src/mod_mutex_init.c
+++ src/mod_mutex_init.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 12:07:42 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/08 12:08:27 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/08 12:47:47 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,13 @@ void	ft_init_mutexes(t_meta *data)
 	ft_init_philo_mutexes(data, data->philos, data->params->num_philos);
 }
 
+/**
+ * @brief Initiates mutexes in philo structs.
+ * 
+ * @param data 			Struct with metadata of the program.
+ * @param philos 		Array of philo structs.
+ * @param num_philos 	Amount of philos.
+ */
 void	ft_init_philo_mutexes(t_meta *data, t_philo *philos, int num_philos)
 {
 	int	i;
@@ -38,7 +45,13 @@ void	ft_init_philo_mutexes(t_meta *data, t_philo *philos, int num_philos)
 	i = -1;
 	while (++i < num_philos)
 	{
-		if (pthread_mutex_init(&philos[i].mtx_status != 0, NULL))
+		if (pthread_mutex_init(&philos[i].mtx_status, NULL) != 0)
+			ft_err_mutex_init(data);
+		if (pthread_mutex_init(&philos[i].mtx_meal_count, NULL) != 0)
+			ft_err_mutex_init(data);
+		if (pthread_mutex_init(&philos[i].mtx_last_meal, NULL) != 0)
+			ft_err_mutex_init(data);
+		if (pthread_mutex_init(&philos[i].left_fork.mtx_taken, NULL) != 0)
 			ft_err_mutex_init(data);
 	}
 }
