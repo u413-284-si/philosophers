7304fd42461a2f83c1a9324bef428282faa2d139
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-08 12:51:21 +0200

    feat: add mutex destroy

--- src/mod_cleanup.c
+++ src/mod_cleanup.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 19:33:37 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/08 12:12:06 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/08 12:38:13 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,39 @@ void	ft_cleanup(t_meta *data)
 		ft_destroy_philo_mutexes(data->philos, data->params->num_philos);
 		free(data->philos);
 	}
+	printf("System cleanup successful. \
+		Can leave now with a peaceful mind. 🙏\n");
 }
 
+/**
+ * @brief Iterates through philos and destroys their mutexes.
+ * 
+ * @param philos 		Array of philo structs.
+ * @param num_philos 	Amount of philos.
+ */
 void	ft_destroy_philo_mutexes(t_philo *philos, int num_philos)
 {
-	
+	int	i;
+
+	i = -1;
+	while (++i < num_philos)
+	{
+		//Check
+		if (pthread_mutex_destroy(&philos[i].mtx_status) != 0)
+			ft_print_err_and_exit(ERR_MUTEX_DESTROY, \
+			"Failed to destroy mtx_status. 😵‍💫\n");
+		//Check
+		if (pthread_mutex_destroy(&philos[i].mtx_meal_count) != 0)
+			ft_print_err_and_exit(ERR_MUTEX_DESTROY, \
+			"Failed to destroy mtx_meal_count. 😵‍💫\n");
+		//Check
+		if (pthread_mutex_destroy(&philos[i].mtx_last_meal) != 0)
+			ft_print_err_and_exit(ERR_MUTEX_DESTROY, \
+			"Failed to destroy mtx_last_meal. 😵‍💫\n");
+		//Check
+		if (pthread_mutex_destroy(&philos[i].left_fork.mtx_taken) != 0)
+			ft_print_err_and_exit(ERR_MUTEX_DESTROY, \
+			"Failed to destroy mtx_fork_taken. 😵‍💫\n");
+	}
 }
+
