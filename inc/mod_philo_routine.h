cfc217a6e56b25c74f4c4feb8a86b45f07af8eb2
sirui <sirui.qiu01@gmail.com>
2023-09-10 18:33:22 +0200

    feat: add philo routine functions

--- inc/mod_philo_routine.h
+++ inc/mod_philo_routine.h
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:48:02 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/09 12:49:01 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/10 17:04:00 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 # include "mod_err_handling.h"
 
 /* ====== DECLARATIONS ====== */
-
+void	*ft_so_lonely(t_philo *philo);
+int		ft_mangiare(t_philo *philo);
+int		ft_dormire(t_philo *philo);
+int		ft_pensare(t_philo *philo);
 
 #endif
--- src/mod_philo_routine.c
+++ src/mod_philo_routine.c
@@ -6,12 +6,25 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:47:34 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/10 11:56:37 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/10 16:54:22 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file mod_philo_routine.c
+ * @brief Functions describing the philosophers routine.
+ */
+
 #include "mod_philo_routine.h"
 
+/**
+ * @brief Defines behaviour of a philo thread.
+ * 
+ * If only one philo is simulated, a particular behaviour is called.
+ * @param arg 		Pointer to argument given to the routine. Here it is
+ * the data struct containing all information regarding the specific philo.
+ * @return void* 	NULL pointer.
+ */
 void	*ft_philo_routine(void *arg)
 {
 	t_philo	*philo;
@@ -21,13 +34,34 @@ void	*ft_philo_routine(void *arg)
 		return (ft_so_lonely(philo));
 	while (true)
 	{
-		
+		if (ft_mangiare(philo) == -1)
+			break ;
+		if (ft_dormire(philo) == -1)
+			break ;
+		if (ft_pensare(philo) == -1)
+			break ;
 	}
+	return (NULL);
 }
 
 void	*ft_so_lonely(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->left_fork.mtx_taken);
 	philo->left_fork.taken = true;
+	return (NULL);
+}
+
+int	ft_mangiare(t_philo *philo)
+{
+	
+}
+
+int	ft_dormire(t_philo *philo)
+{
+	
+}
+
+int	ft_pensare(t_philo *philo)
+{
 	
 }
