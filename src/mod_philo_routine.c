4594719cbd3879efa89c92689af567e16eac63c6
sirui <sirui.qiu01@gmail.com>
2023-09-10 11:57:23 +0200

    add: fill philo routine

--- src/mod_philo_routine.c
+++ src/mod_philo_routine.c
@@ -6,13 +6,28 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:47:34 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/09 12:49:35 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/10 11:56:37 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mod_philo_routine.h"
 
-void	ft_philo_routine(t_meta *data)
+void	*ft_philo_routine(void *arg)
 {
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->params->num_philos == 1)
+		return (ft_so_lonely(philo));
+	while (true)
+	{
+		
+	}
+}
+
+void	*ft_so_lonely(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->left_fork.mtx_taken);
+	philo->left_fork.taken = true;
 	
 }
