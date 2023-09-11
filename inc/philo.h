e7563b1a8725ba6e7b5db49d374f8385e81aae98
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-11 17:08:52 +0200

    chore: add required declarations

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 19:07:06 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/04 18:59:28 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/11 17:06:37 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,16 @@
 
 /* ====== DECLARATIONS ====== */
 
-// input check
+// import from mod_input_check
 void	ft_check_input(int argc, char **argv);
 
-// setup
+// import from mod_setup
 void	ft_setup(int argc, char **argv, t_input *params, t_meta *data);
 
+// import from mod_philo_check
+bool	ft_check_for_dead(t_philo *philos);
+bool	ft_all_fed(t_philo *philos);
+
+// import from mod_cleanup
+void	ft_cleanup(t_meta *data);
 #endif
