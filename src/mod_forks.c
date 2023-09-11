f77d253f249b1a8f39c564f4133d5326958d6fe3
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-11 16:00:12 +0200

    doc: add doxygen

--- src/mod_forks.c
+++ src/mod_forks.c
@@ -6,12 +6,27 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:47:06 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/11 15:17:11 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/11 15:59:58 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file mod_forks.c
+ * @brief Functions to handle forks.
+ */
+
 #include "mod_forks.h"
 
+/**
+ * @brief Pick up fork and print according message.
+ * 
+ * If philo starved while trying to pick up the fork,
+ * the process is cancelled.
+ * Otherwise the bool is set and a message printed.
+ * @param philo 	Philo trying to pick up the fork.
+ * @param fork 		Fork in question.
+ * @return int 		0 on success, -1 on death of philo.
+ */
 int	ft_take_fork(t_philo *philo, t_fork *fork)
 {
 	pthread_mutex_lock(&fork->mtx_taken);
@@ -25,6 +40,11 @@ int	ft_take_fork(t_philo *philo, t_fork *fork)
 	return (0);
 }
 
+/**
+ * @brief Drop fork.
+ * 
+ * @param fork 	Fork in question.
+ */
 void	ft_drop_fork(t_fork *fork)
 {
 	fork->taken = false;
