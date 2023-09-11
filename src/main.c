d9c03e3e61530814ad87159966735c5db75f0447
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-11 17:08:31 +0200

    feat: add check why simulation ended, add cleanup

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 19:35:18 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/08 14:03:54 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/11 17:05:07 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ int	main(int argc, char **argv)
 	ft_check_input(argc, argv);
 	ft_setup(argc, argv, &params, &data);
 	ft_feast(&data);
-/*	if (philo == dead)
-		printf("Another one bites the dust\n");
-	else if (philo == fed)
-		printf("Ich bin so satt, ich mag kein Blatt\n");
-	ft_cleanup(); */
+	if (ft_check_for_dead(data.philos))
+		printf("Another one bites the dust. 😵\n");
+	if (ft_all_fed(data.philos))
+		printf("Ich bin so satt, ich mag kein Blatt. 🐐 \n");
+	ft_cleanup(&data);
 	return (0);
 }
