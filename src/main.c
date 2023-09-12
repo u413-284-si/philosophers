85bea5dfae9d7959c62df862884b2067526487ef
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-12 11:50:46 +0200

    style: add newline before end messages

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 19:35:18 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/11 17:05:07 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/12 11:49:56 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ int	main(int argc, char **argv)
 	ft_setup(argc, argv, &params, &data);
 	ft_feast(&data);
 	if (ft_check_for_dead(data.philos))
-		printf("Another one bites the dust. 😵\n");
+		printf("\nAnother one bites the dust. 😵\n");
 	if (ft_all_fed(data.philos))
-		printf("Ich bin so satt, ich mag kein Blatt. 🐐 \n");
+		printf("\nIch bin so satt, ich mag kein Blatt. 🐐 \n");
 	ft_cleanup(&data);
 	return (0);
 }
