f457bb0f9967bf55c9c434eca3df7181c3978007
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-12 16:59:13 +0200

    fix: add ft_check_for_dead() to header

--- inc/mod_philo_check.h
+++ inc/mod_philo_check.h
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 17:01:51 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/11 11:27:17 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/12 16:58:56 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 bool	ft_starved(t_philo *philo);
 void	ft_is_fed(t_philo *philo);
 bool	ft_all_fed(t_philo *philos);
+bool	ft_check_for_dead(t_philo *philos);
 
 //import from mod_utils.h
 long	ft_get_time(void);
