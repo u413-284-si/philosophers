0dfe466f0ad7a4409eec0d1570b2850c1e119b17
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-09-12 10:52:56 +0200

    feat: add newline to message

--- src/mod_philo_utils.c
+++ src/mod_philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 21:14:40 by sqiu              #+#    #+#             */
-/*   Updated: 2023/09/11 16:30:15 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/09/12 10:43:21 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	ft_declare(t_philo *philo, char *str, bool so_ded)
 	time_elapsed = ft_get_time() - philo->params->start_time;
 	pthread_mutex_unlock(philo->mtx_speak);
 	if (*philo->speak)
-		printf("%ld %d %s", time_elapsed, philo->id, str);
+		printf("%ld %d %s\n", time_elapsed, philo->id, str);
 	if (so_ded)
 		*philo->speak = false;
 	pthread_mutex_unlock(philo->mtx_speak);
