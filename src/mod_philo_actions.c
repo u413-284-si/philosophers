50c3bc74d1f0b4956567743ef974a292eb671c26
sirui <sirui.qiu01@gmail.com>
2023-09-13 23:39:43 +0200

    refactor: separate philo actions from status functions

---
+++ src/mod_philo_actions.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mod_philo_actions.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/10 21:14:40 by sqiu              #+#    #+#             */
+/*   Updated: 2023/09/13 20:23:56 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file mod_philo_utils.c
+ * @brief Functions that philosophers perform occasionally.
+ */
+
+#include "mod_philo.h"
+
+/**
+ * @brief Prints out message regarding philo status.
+ * 
+ * If Boolean speak is set to false by previous philo who died,
+ * then no philo is able to print a status message anymore.
+ * @param philo 	Philo that is speaking.
+ * @param str 		Message stating philo status.
+ * @param so_ded 	Boolean determining if philo is dead.
+ */
+void	ft_declare(t_philo *philo, char *str, bool so_ded)
+{
+	long	time_elapsed;
+
+	time_elapsed = ft_get_time() - philo->params->start_time;
+	pthread_mutex_lock(philo->mtx_speak);
+	if (*philo->speak)
+		printf("%ld %d %s\n", time_elapsed, philo->id, str);
+	if (so_ded)
+		*philo->speak = false;
+	pthread_mutex_unlock(philo->mtx_speak);
+}
+
+/**
+ * @brief Induces calling thread to sleep for specified
+ * timespan (ms).
+ * 
+ * Allows values greater than 1s to be slept for (usleep
+ * only accepts values up to 1.000.000 microsecs).
+ * Approaching the specified timespan incrementally also
+ * increases accuracy as usleep is not guaranteed to 
+ * actually sleep for the demanded time.
+ * @param timespan_to_wait 		Time in ms.
+ */
+void	ft_rest(long timespan_to_wait)
+{
+	long	start;
+	long	elapsed;
+
+	start = ft_get_time();
+	elapsed = 0;
+	while (timespan_to_wait - elapsed > 0)
+	{
+		usleep(10);
+		elapsed = ft_get_time() - start;
+	}
+}
+
+/**
+ * @brief Update meal stats of given philo.
+ * 
+ * Sets last meal to current timestamp.
+ * If check_meals is true, increases meal_count
+ * of philo.
+ * @param philo 	Philo in question.
+ */
+void	ft_set_meal_stats(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->mtx_last_meal);
+	philo->last_meal = ft_get_time();
+	pthread_mutex_unlock(&philo->mtx_last_meal);
+	if (philo->params->check_meals == true)
+	{
+		pthread_mutex_lock(&philo->mtx_meal_count);
+		philo->meal_count++;
+		pthread_mutex_unlock(&philo->mtx_meal_count);
+	}
+}
---
+++ src/mod_philo_status.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mod_philo_status.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 20:13:27 by sqiu              #+#    #+#             */
+/*   Updated: 2023/09/13 23:35:40 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file mod_philo_status.c
+ * @brief Functions to interact with a philosophers status.
+ */
+
+#include "mod_philo.h"
+
+/**
+ * @brief Set the status of the given philo.
+ * 
+ * To ensure atomic setting of the status, its mutex is
+ * first locked and unlocked after the operation.
+ * @param philo 	Philo whose status is to be set.
+ * @param status 	Status to be set in.
+ */
+void	ft_set_status(t_philo *philo, t_status status)
+{
+	pthread_mutex_lock(&philo->mtx_status);
+	philo->status = status;
+	pthread_mutex_unlock(&philo->mtx_status);
+}
+
+/**
+ * @brief Get the status of the given philo.
+ * 
+ * To ensure atomic getting of the status, its mutex is
+ * first locked and unlocked after the operation.
+ * @param philo 	Philo whose status is to be retrieved.
+ * @return status 	Status retrieved.
+ */
+t_status	ft_get_status(t_philo *philo)
+{
+	t_status	status;
+
+	pthread_mutex_lock(&philo->mtx_status);
+	status = philo->status;
+	pthread_mutex_unlock(&philo->mtx_status);
+	return (status);
+}
+
+/**
+ * @brief Set the status of all philos.
+ * 
+ * @param philos 	Pointer to array of philos.
+ * @param status 	Status to be set in.
+ */
+void	ft_set_all(t_philo *philos, t_status status)
+{
+	int	i;
+
+	i = -1;
+	while (++i < philos->params->num_philos)
+		ft_set_status(&philos[i], status);
+}
+
+/**
+ * @brief Iterate through all philos and check their status.
+ * 
+ * Set appropriate Booleans if either
+ * one philo has starved -> dead = true
+ * all philos are fed -> fed = true
+ * @param data 		Struct with metadata of the program.
+ * @param dead 		Boolean to indicate whether a philo has died.
+ * @param fed 		Boolean to indicate whether all philos are fed.
+ */
+void	ft_check_philo_status(t_meta *data, bool *dead, bool *fed)
+{
+	int		i;
+	int		fed_count;
+
+	fed_count = 0;
+	i = -1;
+	while (++i < data->philos->params->num_philos)
+	{
+		if (ft_starved(&data->philos[i]))
+		{
+			*dead = true;
+			break ;
+		}
+		if (data->philos->params->check_meals && ft_is_fed(&data->philos[i]))
+			fed_count++;
+	}
+	if (fed_count == data->philos->params->req_meals)
+		*fed = true;
+}
